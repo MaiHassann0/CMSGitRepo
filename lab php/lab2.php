e5490323efaf3a23737bb9cf6c50356c5eba127f
MaiiHassan <118820284+MaiiHassan@users.noreply.github.com>
2022-12-04 16:58:15 +0200

    Add files via upload

---
+++ lab php/lab2.php
@@ -0,0 +1,48 @@
+<?php
+//new line using /n
+echo nl2br();
+
+//displaying &_SERVER in readable format
+echo "<pre>";
+print($_SERVER ["SERVER_NAME"]);
+echo "</pre>"; 
+
+//display indexed array
+$numbers = array(12, 45, 10, 25);
+
+//sum of array
+array_sum($numbers)
+
+
+//average of array
+$average = array_sum($numbers)/count($numbers);
+echo "average = ". $average. "</br>";
+
+//sorting array reversly from highest to lowest
+krsort(array $numbers);
+
+//associative array
+$information= array("Sara"=>31, "Walaa"=>39, "Ramy"=>40);
+
+//sorting asc acc to value
+asort($information);
+
+//sorting asc acc. to key
+ksort($information);
+
+//sorting desc acc to value
+arsort($information);
+
+//sorting desc acc to key
+krsort($information);
+
+
+
+
+
+
+
+
+
+
+?>
