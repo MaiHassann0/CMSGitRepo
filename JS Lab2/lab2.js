34c5ad2e3ca2b1ee54e767e188a7206f7ee4f15e
MaiiHassan <118820284+MaiiHassan@users.noreply.github.com>
2022-12-11 18:48:40 +0200

    Update lab2.js

--- JS Lab2/lab2.js
+++ JS Lab2/lab2.js
@@ -1,21 +1,29 @@
 //--------part 1--------
-// function LogIn(){
-//     var username = document.getElementsById("username").value;
-//     var password = document.getElementById("password").value;
+function myFunction(){
+    let x ={ userName:"",password:""};
+    x.username=document.getElementById("username").value;
+    x.password=document.getElementById("password").value;
 
-    document.querySelector("#username").onclick = function(){
-    var username = "admin";
-    var password = "123";
+    // document.querySelector("#username").onclick = function(){
+    // var username = "admin";
+    // var password = "123";
+    // }
+    //     if(document.querySelectorAll){
+    //     alert("Welcome");
+    //     return;
+    // }else{
+    //    alert("Not registered"); 
+    // }
 
-        if(document.querySelectorAll){
-        alert("Welcome");
-        return;
-    }else{
-       alert("Not registered"); 
+    if (x.username=="admin"&& x.password==123){
+        document.write("Welcome");
+    } else{
+        document.write("Not registered");
     }
 }
 
 //-------part 2--------
+
 document.querySelector("#add").onclick = function(){
     if(document.querySelector("#newtask input").value.length == 0){
         alert("Enter a todo task");
