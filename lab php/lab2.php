cf86a6ea884c2be30e7a90a71e14feed56be631b
MaiiHassan <118820284+MaiiHassan@users.noreply.github.com>
2022-12-14 00:44:24 +0200

    Update lab2.php

--- lab php/lab2.php
+++ lab php/lab2.php
@@ -1,39 +1,74 @@
+<!-- Lab 2 -->
+
 <?php
+
 //new line using /n
 // nl2br();
 
 //displaying &_SERVER in readable format
 echo "<pre>";
-print($_SERVER ["SERVER_NAME"]);
+print_r($_SERVER ["SERVER_NAME"]);
+// print_r($_SERVER);
 echo "</pre>"; 
+echo '<hr>';
 
-//display indexed array
+//INDEXED ARRAY
 $numbers = array(12, 45, 10, 25);
 
 //sum of array
-array_sum($numbers);
-
+$sum= array_sum($numbers);
+echo 'Sum = '. $sum. '<br>';
+echo '<hr>';
 
 //average of array
 $average = array_sum($numbers)/count($numbers);
-echo "average = ". $average. "</br>";
+echo "Average = ". $average. "<br>";
+echo '<br>';
+echo '<hr>';
 
 //sorting array reversly from highest to lowest
 krsort($numbers);
+foreach($numbers as $values){
+    echo 'Value = '.$values. '<br>';
+    echo '<br>';
+}
+echo '<hr>';
+echo '<hr>';
 
-//associative array
+// -------------------------------
+//ASSOCIATIVE ARRAY
 $information= array("Sara"=>31, "Walaa"=>39, "Ramy"=>40);
 
 //sorting asc acc to value
 asort($information);
+foreach($information as $name => $keys){
+    echo 'Name = '.$name. ' '. 'Value = '. $keys. '<br>';
+    echo '<br>';
+}
+echo '<hr>';
 
 //sorting asc acc. to key
 ksort($information);
+foreach($information as $name => $keys){
+    echo 'Name = '.$name. ' '. 'Value = '. $keys. '<br>';
+    echo '<br>';
+}
+echo '<hr>';
 
 //sorting desc acc to value
 arsort($information);
+foreach($information as $name => $keysAsc){
+    echo 'Name = '.$name. ' '. 'Value = '. $keysAsc. '<br>';
+    echo '<br>';
+}
+echo '<hr>';
 
 //sorting desc acc to key
 krsort($information);
+foreach($information as $name => $keysAsc){
+    echo 'Name = '.$name. ' '. 'Value = '. $keysAsc. '<br>';
+    echo '<br>';
+}
+echo '<hr>';
 
 ?>
