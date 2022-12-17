51c38e42624c94e03dbc0fa708bc8dc552661f9e
MaiiHassan <118820284+MaiiHassan@users.noreply.github.com>
2022-12-17 22:20:21 +0200

    Update Lab4Page2.php

--- lab php/Lab4Page2.php
+++ lab php/Lab4Page2.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html>
 <html>
 <head lang="en">
-        <link rel="stylesheet" href="lab3.css">
+        <link rel="stylesheet" href="lab4.css">
         <meta charset="UTF-8"/>
         <title> User Details </title>
         <meta name="description" content="User Details"/>
@@ -11,18 +11,18 @@
 
  <body>
  
-    <div class="form">
-    <h1 style="color: blue; text-decoration: underline;">
+    <div class = "form">
+    <h1 style="color: blue; text-decoration: underline;"> 
         User Details</h1>
-
-    <table style="border:1px;"> 
+        <a href="Lab4Page1.php" target="_blank"><button >Add new user</button>  </a> 
+    <table style="border:1px solid;"> 
               <tr>
-                <th>User #</th>
-                <th>User Name</th>
-                <th>E-mail</th>
-                <th>Gender</th>
-                <th>Mail Status</th>
-                <th>Action</th>
+                <th style="border:1px solid;">User #</th>
+                <th style="border:1px solid;">User Name</th>
+                <th style="border:1px solid;">E-mail</th>
+                <th style="border:1px solid;">Gender</th>
+                <th style="border:1px solid;">Mail Status</th>
+                <th style="border:1px solid;">Action</th>
               </tr> 
  <?php
 
@@ -30,7 +30,7 @@
 $dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = '';
-$dbname = 'User_Data';
+$dbname = 'app_form';
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
 // if(! $conn){
@@ -55,14 +55,14 @@ $result=mysqli_query($conn,$sql);
 if (mysqli_num_rows($result) > 0){ 
 
    while ($row = mysqli_fetch_assoc($result)){
-    echo "<tr>
-    <td>" . $row["User_no"]. "</td>
-    <td>" . $row["User_name"]. "</td>
-    <td>" . $row["Email"]. "</td>
-    <td>" . $row["Gender"]. "</td>
-    <td>" . $row["Mail_status"]. "</td>
-    <td><a href='lab4Page1.php'>view</a></td>
-    </tr> ";
+       echo '<tr>';
+    echo '<td style="border:1px solid;">' . $row["User_no"]. '</td>';
+    echo'<td style="border:1px solid;">' . $row["User_name"]. '</td>';
+    echo '<td style="border:1px solid;">' . $row["Email"]. '</td>';
+    echo '<td style="border:1px solid;" >' . $row["Gender"]. '</td>';
+    echo '<td style="border:1px solid;" >';  if ($row["Mail_status"]=="on"){echo "Yes";}else{echo "No";} echo '</td>';
+    echo '<td style="border:1px solid;"><a href="lab4Page3.php" target="_blank">View</a></td>';
+       echo '</tr>';
     }
 } 
 mysqli_close($conn);
@@ -70,3 +70,4 @@ mysqli_close($conn);
 // echo "Data fetched successfully\n";
 
 ?>
+
